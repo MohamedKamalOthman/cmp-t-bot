f7e42127c9dfa3f0a100198b5d64f51f268e58af
Mohamed Kamal <medoking91@gmail.com>
2023-02-24 14:15:36 +0200

    main.py ready for deployment

--- main.py
+++ main.py
@@ -28,13 +28,16 @@ def send_welcome(message):
 @bot.message_handler(content_types=['document'])
 def upload_documents(message):
     # Only upload media from materials group
+    # Be careful because telegram char groups ids may change
     if message.chat.id != GROUP_ID:
         return
 
     # logs for monitoring
     print(message.document)
     doc = message.document
-    file_name, file_id, mime_type = doc.file_name, doc.file_id, doc.mime_type
+    file_name, file_id = doc.file_name, doc.file_id
+
+    # reply in chat the that the bot has received the message remove this if you want the bot to be silent
     bot.reply_to(message, f'Got it {file_name}')
     file_path = bot.get_file(file_id).file_path
 
@@ -51,21 +54,34 @@ def upload_documents(message):
         'data': ('metadata', json.dumps(param), 'application/json;charset=UTF-8'),
         'file': response.content
     }
+
     # upload file to drive
     r = requests.post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
                       headers={"Authorization": "Bearer " + g_auth.credentials.access_token},
                       files=file
                       )
+
+    # Refresh google token if expired then try again (Unauthorized)
+    if r.status_code == 401:
+        g_auth.Refresh()
+        r = requests.post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+                          headers={"Authorization": "Bearer " + g_auth.credentials.access_token},
+                          files=file
+                          )
+
     # logs for monitoring
     print(r.text)
 
     # Send me a message with status code to the user id
-    bot.send_message(chat_id=USER_ID, text=f'Trying to upload {file_name} ended with status {r.status_code}', )
+    bot.send_message(chat_id=USER_ID,
+                     text=f'Trying to upload {file_name} ended with status {r.status_code} was sent'
+                          f' from {message.chat.first_name} {message.chat.last_name}', )
+
     # have some rest we don't want our free server to crash :)
     time.sleep(5)
 
 
 print("bot has started")
 
-# poll every 10 seconds
-bot.polling(interval=10)
+# poll infinitely
+bot.infinity_polling()
